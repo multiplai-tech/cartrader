bbc29de35a65f3dba42820a292c63fbe4461df8f
Raffy <info.johnlim@gmail.com>
2024-08-17 03:31:49 +0800

    chore: created car description page template

--- app.vue
+++ app.vue
@@ -5,53 +5,62 @@
 		<a class="text-3xl font-mono space-x-6" href="#">Cartrader</a>
 	</header>
 	<!-- NAVBAR -->
-	
-	<!-- CARS PAGE -->
-	<div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-		<div class="mt-32 flex">
-			<!-- CAR SIDE BAR -->
-			<div class="shadow border w-64 mr-10 z-30 h-[190px]">
-				<div class="p-5 flex justify-between relative cursor-pointer border-b">
-					<h3>Location</h3>
-					<h3 class="text-blue-400 capitalize">Cavite</h3>
-					<!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-						<input type="text" class="border p-1 rounded" />
-						<button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
-					</div> -->
-				</div>
 
-				<div class="p-5 flex justify-between relative cursor-pointer border-b">
-					<h3>Make</h3>
-					<h3 class="text-blue-400 capitalize">Toyota</h3>
-				</div>
-				
-				<div class="p-5 flex justify-between relative cursor-pointer border-b">
-					<h3>Price</h3>
-					<h3 class="text-blue-400 capitalize">Any</h3>
-				</div>
-			</div>
-			<!-- CAR SIDE BAR -->
-			
-			<!-- CAR CARDS -->
-			<div class="w-full">
-				<!-- CAR CARD -->
-				<div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
-					<div class="flex h-full">
-						<img src="https://carconfigurator.ferrari.com/rt-assets/data/cars/812competizione-a/ui/splashpage.jpg" class="w-[300px] h-full bg-fixed" />
-						<div class="p-4 flex flex-col">
-							<div>
-								<h1 class="text-2xl text-blue-700">Volvo XC40</h1>
-								<p class="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-							</div>
-							<h1 class="mt-auto text-xl">$3,900.55</h1>
-						</div>
+	<!-- CAR DETAIL PAGE -->
+	<div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+		<div>
+			<!-- CAR HERO -->
+			<div class="mt-10">
+				<img src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg" class="w-full" alt="" />
+				<h1 class="mt-10 text-4xl">Volvo 3XT</h1>
+				<div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+					<div class="flex">
+						<p class="mr-2">5 seats</p>
+						<p class="mr-2">|</p>
+						<p class="mr-2">67,444 miles</p>
+					</div>
+					<div>
+						<p class="font-bold text-2xl">$25,555</p>
 					</div>
 				</div>
-				<!-- CAR CARD -->
 			</div>
-			<!-- CAR CARDS -->
 		</div>
+		<!-- CAR HERO -->
+		<!-- CAR ATTRIBUTES -->
+		<div class="mr-10 mt-5 border-b pb-5">
+			<div class="flex text-lg mt-2">
+				<p class="rounded text-lime-800 mr-3">✔</p>
+				<p>Leather Interior</p>
+			</div>
+		</div>
+		<!-- CAR ATTRIBUTES -->
+		<!-- CAR DESCRISPTION -->
+		<div class="mt-5">
+			<p class="mb-10">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+				consequuntur, et saepe sequi, ipsum quisquam iste autem quas vel aperiam
+				laboriosam dolorum vero tempore placeat, asperiores alias magni. Nulla,
+				dolores.
+			</p>
+		</div>
+		<!-- CAR DESCRISPTION -->
+		<!-- CAR CONTACT -->
+		<div class="mt-10">
+			<div class="flex w-[600px] justify-between">
+				<input type="text" class="border p-1" placeholder="Name" />
+				<input type="text" class="border p-1" placeholder="Email" />
+				<input type="text" class="border p-1" placeholder="Phone" />
+			</div>
+			<div class="flex mt-4 w-[600px]">
+				<textarea class="border p-1 w-full" placeholder="Message"></textarea>
+			</div>
+			<button class="bg-blue-400 text-white px-10 py-3 rounded mt-4">
+				Submit
+			</button>
+		</div>
+		<!-- CAR CONTACT -->
 	</div>
-	<!-- CARS PAGE -->
+	<!-- CAR DETAIL PAGE   -->
+
 </div>
 </template>
