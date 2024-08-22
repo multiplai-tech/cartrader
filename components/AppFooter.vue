ff842c73668b4b2175d01ab24ae7638a718c0cdb
Raffy <info.johnlim@gmail.com>
2024-08-22 22:42:26 +0800

    chore: Updated responsiveness across all breakpoints

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
-	Footah
+<div class="mt-10 bg-[#0f151b] text-white p-4 px-20">
+	LoremIpsum 2025
 </div>
 </template>
--- components/Car/Hero.vue
+++ components/Car/Hero.vue
@@ -20,28 +20,28 @@ useHead({
 			<CarSearchBar />
 		</div>
 	</div>
-	<div class="hidden md:block lg:block flex flex-col justify-center mb-20">
+	<div class="hidden md:block lg:block flex-col justify-center mb-20">
 		<div class="flex flex-col text-center justify-center text-[#d3d3d3] text-[20rem] barlow-condensed-extrabold">
 			<p>LOREM IPSUM</p>
 		</div>
-		<div class="-mt-72 flex justify-center">
+		<div class="-mt-72 md:-mt-[750px] lg:-mt-72 flex justify-center">
 			<NuxtImg src="/img/car-1.png" class="aspect-auto h-[600px]" />
 		</div>
 	</div>
-	<div class="grid mt-10 md:mt-0 lg:grid-cols-3 auto-rows-max gap-2 w-full h-auto px-2 lg:px-16">
+	<div class="grid mt-10 md:mt-36 lg:mt-10 lg:grid-cols-3 auto-rows-max gap-2 w-full h-auto px-2 lg:px-16">
 		<div class="h-[300px] overflow-hidden rounded-xl lg:max-h-[70vh] lg:order-none lg:h-screen lg:col-span-2">
-			<div class="transition-transform duration-300 transform scale-150 bg-[center_100px] peer h-screen lg:h-screen bg-no-repeat bg-contain bg-[url('https://images.unsplash.com/photo-1592840062661-a5a7f78e2056?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] lg:bg-[center_-200px]"></div>
+			<div class="transition-transform duration-300 transform scale-150 md:scale-110 lg:scale-110 bg-[center_100px] md:bg-[center_-40px] lg:bg-[center_0px] peer h-screen lg:h-screen bg-no-repeat bg-contain bg-[url('https://images.unsplash.com/photo-1592840062661-a5a7f78e2056?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"></div>
 		</div>
 		<div class="bg-[#1c1c1c] text-white rounded-xl flex flex-col w-100 lg:h-[70vh] p-5 lg:p-10">
-			<p class="text-3xl mb-10 lg:text-6xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+			<p class="text-3xl mb-10 lg:text-5xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
 			<p class="text-1xl lg:text-2xl font-bold mt-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quam! Dolor sit amet consectetur.</p>
 		</div>
 		<div class="order-2 lg:order-none bg-[#1c1c1c] text-white rounded-xl flex flex-col w-100 lg:h-[70vh] p-5 lg:p-10">
-			<p class="text-3xl mb-10 lg:text-6xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+			<p class="text-3xl mb-10 lg:text-5xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
 			<p class="text-1xl lg:text-2xl font-bold mt-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quam! Dolor sit amet consectetur.</p>
 		</div>
 		<div class="h-[300px] overflow-hidden rounded-xl order-1 lg:max-h-[70vh] lg:order-none lg:h-screen lg:col-span-2">
-			<div class="transition-transform duration-300 transform scale-150 bg-[center_60px] peer h-screen lg:h-screen bg-no-repeat bg-contain bg-[url('https://images.unsplash.com/photo-1537984822441-cff330075342?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] lg:bg-[center_-200px]"></div>
+			<div class="transition-transform duration-300 transform scale-150 bg-[center_00px] md:bg-[center_-180px] lg:bg-[center_-200px] peer h-screen lg:h-screen bg-no-repeat bg-contain bg-[url('https://images.unsplash.com/photo-1537984822441-cff330075342?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"></div>
 		</div>
 	</div>
 </div>
