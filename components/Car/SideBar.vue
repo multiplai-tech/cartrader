b4e5d96080736ce5f0ba2e18abc3648daec08300
Raffy <info.johnlim@gmail.com>
2024-08-22 22:15:05 +0800

    chore: Make price function

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,7 +1,26 @@
 <script setup>
 const { makes } = useCars()
 const route = useRoute()
+const router = useRouter();
 const city = ref('')
+const priceRangeText = computed(() => {
+	const minPrice = route.query.minPrice
+	const maxPrice = route.query.maxPrice
+
+	if (!minPrice && !maxPrice) return 'Any'
+	else if (!minPrice && maxPrice) {
+		return `< $${ maxPrice }`
+	}
+	else if (minPrice && !maxPrice) {
+		return `> $${ minPrice }`
+	} else {
+		return `$${ minPrice }-$${ maxPrice }`
+	}
+})
+const priceRange = ref({
+	min: "",
+	max: "",
+})
 
 const modal = ref({
 	make: false,
@@ -29,6 +48,20 @@ const handleLocation = () => {
 const onChangeMake = (make) => {
 	updateModal('make')
 	navigateTo(`/city/${route.params.city}/car/${make}`)
+}
+
+const onChangePrice = () => {
+	updateModal('price')
+	if (priceRange.value.max && priceRange.value.min) {
+		if (priceRange.value.min > priceRange.value.max) return
+	}
+
+	router.push({
+		query: {
+			minPrice: priceRange.value.min,
+			maxPrice: priceRange.value.max,
+		}
+	})
 }
 </script>
 
@@ -59,9 +92,16 @@ const onChangeMake = (make) => {
 	</div>
 	<!-- MAKE END -->
 
+	<!-- PRICE START -->
 	<div class="p-5 flex justify-between relative cursor-pointer border-b">
 		<h3>Price</h3>
-		<h3 class="text-blue-400 capitalize"></h3>
+		<h3 class="text-blue-400 capitalize" @click="updateModal('price')">{{ priceRangeText }}</h3>
+		<div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white" v-if="modal.price">
+			<input class="border p-1 rounded" type="number" placeholder="Min" v-model="priceRange.min" />
+			<input class="border p-1 rounded" type="number" placeholder="Max" v-model="priceRange.max" />
+			<button class="bg-blue-400 w-full mt-2 rounded text-white p-1" @click="onChangePrice">Apply</button>
+		</div>
 	</div>
+	<!-- PRICE END -->
 </div>
 </template>
