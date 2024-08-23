ecfa19141f23ac2ec5ad4b78fd4ff9fafe830b0d
Raffy <info.johnlim@gmail.com>
2024-08-24 02:28:01 +0800

    fix: Added runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@vueuse/nuxt', "@nuxtjs/supabase"],
-  tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'first' }],
-    configPath: 'tailwind.config',
-    exposeConfig: {
-      level: 2
-    },
-    config: {},
-    viewer: true,
-  },
-  supabase: {
-	url: process.env.SUPABASE_URL,
-	key: process.env.SUPABASE_KEY,
-    redirect: false,
-  }
+	compatibilityDate: '2024-04-03',
+	runtimeConfig: {
+		public: {
+			url: process.env.SUPABASE_URL,
+			key: process.env.SUPABASE_KEY,
+		}
+	},
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@vueuse/nuxt', "@nuxtjs/supabase"],
+	tailwindcss: {
+		cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'first' }],
+		configPath: 'tailwind.config',
+		exposeConfig: {
+			level: 2
+		},
+		config: {},
+		viewer: true,
+	},
+	supabase: {
+		redirect: false,
+	}
 })
